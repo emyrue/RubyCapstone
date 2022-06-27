42732e15d545fa54084e383cd4ec61be1ca7c963
emyrue <94935633+emyrue@users.noreply.github.com>
2022-06-27 16:41:34 -0600

    Changed Item class to take fewer parameters

--- classes/game.rb
+++ classes/game.rb
@@ -1,8 +1,8 @@
 require_relative 'item'
 
 class Game < Item
-  def initialize(genre, author, source, label, date, multiplayer, last_played_at)
-    super(genre, author, source, label, date)
+  def initialize(date, multiplayer, last_played_at)
+    super(date)
     @multiplayer = multiplayer
     @last_played_at = last_played_at
   end
