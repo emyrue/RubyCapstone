45de583e2035a99bc33d7a9310ae6d8e84edbc99
Alexr16 <j.alex-1996@hotmail.com>
2022-06-27 17:31:23 -0500

    Initialize constructor

--- classes/book.rb
+++ classes/book.rb
@@ -1,7 +1,11 @@
 require_relative 'item'
 
 class Book < Item
-    def initialize 
-        @
+    def initialize(publisher, cover_state)
+        super()
+        @publisher = publisher
+        @cober_state = cover_state
     end
+
+    
 end
