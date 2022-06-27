0863001f1bea6ae3a97b44190e5f15db508b7ccc
isaacpitwa <isaacpitwa256@gmail.com>
2022-06-28 01:31:28 +0300

    Define Music Album class

--- classes/music_album.rb
+++ classes/music_album.rb
@@ -1,4 +1,12 @@
 require_relative 'classes/item'
 class MusicAlbum:Item
-    def initialize
+    def initialize(on_spotify,cover_state)
+        @on_spotify = on_spotify
+        @cover_state = cover_state
+        super()
+    end
+
+    def can_be_archived
+        super() && @on_spotify
+    end
 end
