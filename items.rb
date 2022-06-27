dd70c9bd0ed81f8065cafa668aee42968276b6a2
isaacpitwa <isaacpitwa256@gmail.com>
2022-06-27 23:12:42 +0300

    implement Item  class methods  and

--- items.rb
+++ items.rb
@@ -1,15 +1,25 @@
 require 'securerandom'
+require 'date'
 
-class Items
-    attr_accessor :publish_date
-    attr_reader :genre, :author, :label, :source
-    def initialize(genre, author, source, label, date)
-        @id = SecureRandom.uuid
-        @genre = genre
-        @author = author
-        @source = source
-        @label = label
-        @publish_date = date
-        @archived = false
-    end
+class Item
+  attr_accessor :publish_date
+  attr_reader :genre, :author, :label, :source
+
+  def initialize(genre, author, source, label, date)
+    @id = SecureRandom.uuid
+    @genre = genre
+    @author = author
+    @source = source
+    @label = label
+    @publish_date = date
+    @archived = false
+  end
+
+  def can_be_archived?
+    Date.today.year - Date.parse(@publish_date).year > 10
+  end
+
+  def move_to_archive
+    @archived = true if can_be_archived?
+  end
 end
