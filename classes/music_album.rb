4b51a79284b10921a6d0bc53bbe68283de486a44
isaacpitwa <isaacpitwa256@gmail.com>
2022-06-28 01:49:18 +0300

    Add Updates

--- classes/music_album.rb
+++ classes/music_album.rb
@@ -1,12 +1,11 @@
-require_relative 'classes/item'
+require_relative 'item'
 class MusicAlbum < Item
+  def initialize(on_spotify)
+    @on_spotify = on_spotify
+    super()
+  end
 
-    def initialize(on_spotify)
-        @on_spotify = on_spotify
-        super()
-    end
-
-    def can_be_archived
-        super() && @on_spotify
-    end
+  def can_be_archived
+    super() && @on_spotify
+  end
 end
