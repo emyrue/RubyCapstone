49eb07ef309c828b7299742c74f65a19f6407436
Alexr16 <j.alex-1996@hotmail.com>
2022-06-27 17:50:20 -0500

    Fix linter errors

--- classes/book.rb
+++ classes/book.rb
@@ -1,14 +1,15 @@
 require_relative 'item'
 
 class Book < Item
-    def initialize(date, publisher, cover_state)
-        super(date)
-        @publisher = publisher
-        @cover_state = cover_state
-    end
-    
-    def can_be_archived?
-        return true if super || @cover_state == 'bad'
-        false
-    end
+  def initialize(date, publisher, cover_state)
+    super(date)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
+
+  def can_be_archived?
+    return true if super || @cover_state == 'bad'
+
+    false
+  end
 end
