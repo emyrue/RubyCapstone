8afedda732aa28d7fca94b0901e6adf2187c2d4a
emyrue <94935633+emyrue@users.noreply.github.com>
2022-06-28 16:24:19 -0600

    Most tests for Author class completed

--- tests/author_spec.rb
+++ tests/author_spec.rb
@@ -1,1 +1,28 @@
 require_relative '../classes/author'
+require_relative '../classes/book'
+
+describe Author do
+  context 'When testing Author class' do
+    before :each do
+      @author = Author.new('Emily', 'Robertson')
+      @book = Book.new('2022/06/28', 'Juan.inc', 'Good')
+    end
+
+    it 'Returns instance of Author' do
+      expect(@author).to be_instance_of(Author)
+    end
+
+    it 'Has correct first name' do
+      expect(@author.first_name).to eq 'Emily'
+    end
+
+    it 'Has correct last name' do
+      expect(@author.last_name).to eq 'Robertson'
+    end
+
+    it 'Item adds itself to author.items array' do
+      @book.add_author(@author)
+      expect(@author.items).not_to be_empty
+    end
+  end
+end
