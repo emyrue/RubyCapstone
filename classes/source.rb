765c6d6b5c20ae76402d5c9df68821def85277ed
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-06-28 10:22:28 -0500

    can_be_archived? added

--- classes/source.rb
+++ classes/source.rb
@@ -1,15 +1,14 @@
 require 'Securerandom'
 
 class Source
+  def initialize(name)
+    @id = SecureRandom.uuid
+    @name = name
+    @items = []
+  end
 
-    def initialize (name)
-        @id = SecureRandom.uuid
-        @name = name
-        @items = []
-    end
-
-    def add_item(item)
-        @items << item
-        item.add_source(self)
-    end
+  def add_item(item)
+    @items << item
+    item.add_source(self)
+  end
 end
