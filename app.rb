d62bc3a869d6031fe073798ce2525e2635adf13c
emyrue <94935633+emyrue@users.noreply.github.com>
2022-06-28 11:04:08 -0600

    Fixed linter errors

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ require_relative 'classes/source'
 require_relative 'modules/attributes'
 
 class App
-  include Attributes 
+  include Attributes
   def initialize
     @books = []
     @music = []
@@ -19,16 +19,14 @@ class App
   end
 
   def add_item
-    puts "When was this item published?"
+    puts 'When was this item published?'
     publish_date = gets.chomp
     author = item_author
     label = item_label
     source = item_source
     genre = item_genre
-    return {publish_date: publish_date, author: author, label: label, source: source, genre: genre}
+    { publish_date: publish_date, author: author, label: label, source: source, genre: genre }
   end
 
-  def list_labels
-
-  end
+  def list_labels; end
 end
--- modules/attributes.rb
+++ modules/attributes.rb
@@ -4,10 +4,10 @@ require_relative '../classes/source'
 
 module Attributes
   def item_author
-    puts "Would you like to (1)add a new author or (2)choose an author that is already on the list?"
+    puts 'Would you like to (1)add a new author or (2)choose an author that is already on the list?'
     choice = gets.chomp.to_i
     while choice != 1 && choice != 2
-      puts "Please enter a valid option: "
+      puts 'Please enter a valid option: '
       choice = gets.chomp.to_i
     end
     if choice == 1
@@ -17,65 +17,65 @@ module Attributes
       last_name = gets.chomp
       author = Author.new(first_name, last_name)
     else
-      puts "List of authors"
+      puts 'List of authors'
       author = @authors[0]
     end
-    return author
+    author
   end
 
   def item_label
-    puts "Would you like to (1)add a new label or (2)choose a label that is already on the list?"
+    puts 'Would you like to (1)add a new label or (2)choose a label that is already on the list?'
     choice = gets.chomp.to_i
     while choice != 1 && choice != 2
-      puts "Please enter a valid option: "
+      puts 'Please enter a valid option: '
       choice = gets.chomp.to_i
     end
     if choice == 1
-      puts "What is the label name?"
+      puts 'What is the label name?'
       label_name = gets.chomp
-      puts "What is color?"
+      puts 'What is color?'
       label_color = gets.chomp
       label = Label.new(label_name, label_color)
     else
-      puts "List of labels"
+      puts 'List of labels'
       label = @labels[0]
     end
-    return label
+    label
   end
 
   def item_source
-    puts "Would you like to (1)add a new source or (2)choose a source that is already on the list?"
+    puts 'Would you like to (1)add a new source or (2)choose a source that is already on the list?'
     choice = gets.chomp.to_i
     while choice != 1 && choice != 2
-      puts "Please enter a valid option: "
+      puts 'Please enter a valid option: '
       choice = gets.chomp.to_i
     end
     if choice == 1
-      puts "What is the source name?"
+      puts 'What is the source name?'
       source_name = gets.chomp
       source = Source.new(source_name)
     else
-      puts "List of sources"
+      puts 'List of sources'
       source = @sources[0]
     end
-    return source
+    source
   end
 
   def item_genre
-    puts "Would you like to (1)add a new genre or (2)choose a genre that is already on the list?"
+    puts 'Would you like to (1)add a new genre or (2)choose a genre that is already on the list?'
     choice = gets.chomp.to_i
     while choice != 1 && choice != 2
-      puts "Please enter a valid option: "
+      puts 'Please enter a valid option: '
       choice = gets.chomp.to_i
     end
     if choice == 1
-      puts "What is the genre name?"
+      puts 'What is the genre name?'
       genre_name = gets.chomp
       genre = Genre.new(genre_name)
     else
-      puts "List of genres"
+      puts 'List of genres'
       genre = @genres[0]
     end
-    return genre
+    genre
   end
 end
