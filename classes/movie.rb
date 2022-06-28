de19abbe38b89b3486863211ae4afe070fc37370
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-06-28 10:15:45 -0500

    Movie method done

--- classes/movie.rb
+++ classes/movie.rb
@@ -1,12 +1,12 @@
 require_relative 'item'
 
-class Movie
-
+class Movie < Item
     def initialize (date, silent)
         super(date)
         @silent = silent
     end
 
-    
-
+    def can_be_archived
+        super || @silent
+    end
 end
