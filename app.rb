e3f70d7efe2eb96a3bb269640f2627e31515ddaa
isaacpitwa <isaacpitwa256@gmail.com>
2022-06-28 19:06:44 +0300

    Complete List genres

--- app.rb
+++ app.rb
@@ -10,10 +10,34 @@ class App
     @sources = []
   end
 
+
+  def item_input(item_type)
+    print 'What Date was it Published : '
+    date = gets.chomp
+    if item_type == '1'
+      # Handle Music Albums Inputs
+      print 'Can the Album be found using Spotify? [Y/N]: '
+      on_spotify = on_spotify?
+      [date, on_spotify]
+    else
+      print 'Not yet supported :)'
+    end
+  end
+
+
   def list_albums
       return puts "\n>>>> No Music  Albums available<<<" if @music.empty?
       puts
-      @music.each { |album| puts "ID: #{album.id}\t\tGenre: #{album.genre.name} \t\t On Spotify: #{album.on_spotify} " }
+      @music.each { |album| puts "ID: #{album.id}\t\tGenre: #{album.genre.name} \t\t On Spotify: #{album.on_spotify} \t\t Pubished On : #{album.publish_date} " }
       puts
   end 
+
+
+  def list_genres
+    return puts "\n>>>> No Genres  available<<<" if @genres.empty?
+    puts
+    @genres.each { |genre| puts "ID: #{genre.id}\t\tGenre: #{genre.name} " }
+    puts
+end 
+
 end
