ac0b31242cb30445f56565f40ff80a6194f8e340
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-06-28 14:25:07 -0500

    linters

--- classes/create_movie.rb
+++ classes/create_movie.rb
@@ -1,10 +1,10 @@
 require_relative 'movie'
 
 class MovieGenerator
-    def create_movie(date)
-        puts 'Is this movie silent? y/n'
-        silent = gets.chomp.to_s == 'y' ? true : false
-        puts 'Movie created successfully!'
-        Movie.new(date, silent)
-    end
+  def create_movie(date)
+    puts 'Is this movie silent? y/n'
+    silent = gets.chomp.to_s == 'y'
+    puts 'Movie created successfully!'
+    Movie.new(date, silent)
+  end
 end
--- main.rb
+++ main.rb
@@ -25,7 +25,7 @@ class Main
     gets.chomp
   end
 
-  def choose(choice)
+  def choose(choice) # rubocop:disable Metrics/CyclomaticComplexity
     case choice.to_i
     when 1
       @app.list_all_books
