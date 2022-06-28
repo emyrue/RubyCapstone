abb4db4565a9e5fb28aa3c07237ee873f0d6bff1
Alexr16 <j.alex-1996@hotmail.com>
2022-06-28 12:57:07 -0500

    Refactor bookgenerator

--- classes/create_book.rb
+++ classes/create_book.rb
@@ -1,13 +1,11 @@
 require_relative 'book'
 
 class BookGenerator
-  def create_book
+  def create_book(date)
     print 'Publisher: '
     publisher = gets.chomp.to_s
     print 'Cover State: '
     cover_state = gets.chomp.to_s
-    print 'Date: '
-    date = gets.chomp
     puts 'Book created successfully!'
     Book.new(date, publisher, cover_state)
   end
